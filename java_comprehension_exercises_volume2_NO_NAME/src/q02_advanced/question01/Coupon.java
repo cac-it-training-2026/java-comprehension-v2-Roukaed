c8129e355d9b050452b12527dbaca1933259b395
uedashi <uedashi@C010422996.zipangu.cac.co.jp>
2026-04-28 16:09:53 +0900

    0428 q01_basic.question07まで完了
    理解出来た点
    ・自らクラスやメソッドを作成し、mainメソッドで動かすことができた。
    
    不明な点
    ・q01_basic.question07の
    
    int half = numbers.size() / 2;
    		for (int i = 0; i < half; i++) {
    			numbers.remove(0);
    		}
    
    としたときと、
    
    for (int i = 0; i < numbers.size() / 2; i++) {
    			numbers.remove(0);
    		}
    
    としたときの挙動の違い

--- java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question01/Coupon.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question01/Coupon.java
@@ -1,5 +1,21 @@
 package q02_advanced.question01;
 
 public class Coupon {
+	int id;
+	double discountRate;
+	String discription;
+
+	public Coupon() {
+	}
+
+	public Coupon(int id, double discountRate, String discription) {
+		this.id = id;
+		this.discountRate = discountRate;
+		this.discription = discription;
+	}
+
+	public String toString() {
+		return "null";
+	}
 
 }
