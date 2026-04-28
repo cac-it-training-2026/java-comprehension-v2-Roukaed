5a9b530fb0a58ae1782b96b68bc0a2561df39ad4
uedashi <uedashi@C010422996.zipangu.cac.co.jp>
2026-04-28 16:06:18 +0900

    0428 q1_basic.question07まで完了
    理解できた点
    ・自らクラスやメソッドを作成し、mainメソッドを動かすことができた。
    
    不明な点
    ・q1_basic.question07の
    for(int i = 0;i < number.size() / 2 ;i++)
    	numbers.remove(0);
    
    としたときと、
    
    int half = numbers.size() / 2;
    		for (int i = 0; i < half; i++) {
    			numbers.remove(0);
    		}

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
@@ -3,8 +3,14 @@ package q01_basic.question03;
 public class SystemMain03 {
 
 	public static void main(String[] args) {
-		//TODO ここから実装する
+		Member member = new Member();
+		Member member2 = new Member("Miura Manabu", 28, 2);
 
+		System.out.println("---SHOW DATA(NULL)---");
+		member.showMember();
+
+		System.out.println("---SHOW DATA(Miura)---");
+		member2.showMember();
 	}
 
 }
